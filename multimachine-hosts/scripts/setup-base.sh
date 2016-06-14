c448c94a820767f52db50060a5ffdf516ef448ac
Joaquin Menchaca <jmenchaca@gobalto.com>
2016-06-13 17:53:50 -0700

    hosts comment update

--- multimachine-hosts/scripts/setup-base.sh
+++ multimachine-hosts/scripts/setup-base.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-# NAME: setup-all.sh
+# NAME: setup-base.sh
 # AUTHOR: Joaquin Menchaca
 # CREATED: 2015-11-23
 # UPDATED:
-#   * 2016-04-24
+#   * 2016-04-24 support multiple data formats
 #   * 2016-06-13 built library to support any file
 #
 # PURPOSE: Configures `/etc/hosts` and global ssh configuration for each
 #  password-less system to system communication through ssh.
 # DEPENDENCIES:
-#  * GNU Bash 3+, POSIX Commands (cut, grep, tr)
-#  * Global Configuration - global.hosts
-#  * VirtualBox Guest Editions installed on guest system
-#  * Local host . directory mounted as /vagrant on guest system
+#  * baselib.src
 # NOTES:
 #  * This script will be run on the guest operating system
 

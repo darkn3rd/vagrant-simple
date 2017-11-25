119f00381b4abcb3de140f966846599847a99130
Joaquin Menchaca <joaquin.menchaca@indiegogo.com>
2017-11-25 15:16:04 -0800

    ServerSpec for SingleMachine

--- A01.singlemachine/README.md
+++ A01.singlemachine/README.md
@@ -19,3 +19,19 @@ This is static single machine configuration that demonstrates basics options con
 vagrant up      # start system and provision it
 vagrant ssh     # to ssh into system
 ```
+
+## **Testing with ServerSpec**
+
+You can test using ServerSpec.  You'll have to install ServerSpec (`gem install serverspec`). These are only tested from Mac OS X host with Ruby 2.4.1.
+
+You can run these using the test harness:
+
+```bash
+rake spec
+```
+
+Or you can manually orchestrate it and use:
+
+```bash
+TARGET_HOST=default rspec
+```
---
+++ A01.singlemachine/spec/default/singlemachine_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+# Test Configuratiosn from Provisioning
+
+describe 'Provisioning Script Tests' do
+  describe ppa('dawidd0811/neofetch-daily') do
+    it { should exist }
+    it { should be_enabled }
+  end
+
+  describe package('neofetch') do
+    it { should be_installed }
+  end
+
+  describe package('landscape-common') do
+    it { should_not be_installed }
+  end
+
+  describe file('/etc/update-motd.d/20-neofetch') do
+    it { should be_linked_to '/usr/bin/neofetch' }
+    it { should be_executable }
+  end
+end
+
+# Default Configurations From Vagrantfile
+describe 'Vagrantfile Configuration Tests' do
+  describe interface('eth1') do
+    it { should have_ipv4_address('192.168.53.72') }
+  end
+
+  describe host('myworkstation.dev') do
+    its(:ipaddress) { should eq '127.0.0.1' }
+    it { should be_resolvable.by('hosts') }
+  end
+end
+
+describe 'Vagrant Image Tests' do
+  describe user('vagrant') do
+    it { should exist }
+    it { should belong_to_group 'vagrant' }
+  end
+end
